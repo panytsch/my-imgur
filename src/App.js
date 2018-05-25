d6576e842368284e68b50d8c0c8e483750b49c70
panytsch <romeo.crary@gmail.com>
2018-05-25 23:23:10 +0300

    just play with range of downloaded data

--- src/App.js
+++ src/App.js
@@ -4,52 +4,65 @@ import { withRouter } from "react-router-dom";
 import axios from "axios";
 const url = "https://api.imgur.com/3/gallery/hot/top/day/1?album_previews=true";
 const settings = {
-  headers: {
-    authorization: "Client-ID d957cd89bbfd69b"
-  }
+	headers: {
+		authorization: "Client-ID d957cd89bbfd69b"
+	}
 };
 class App extends Component {
-  state = { data: [] };
-  componentWillMount() {
-    axios
-      .get(url, settings)
-      .then(response => {
-        this.props.handleClick(response.data.data);
-        // this.setState(this.state.data, response.data.data)
-      })
-      .catch(function(error) {
-        console.log("Request failed", error);
-      });
-  }
-  render() {
-    const { datas } = this.props;
-    console.log(datas[0]);
-    return (
-      <div>
-        {datas[0] &&
-          datas[0].map((i, key) => (
-            <p key={key}>
-              {(i.link && <a href={i.link}>{i.id}</a>) ||
-                (i.images && <a href={i.images[0].link}>{i.id}</a>) ||
-                i.id}
-            </p>
-          ))}
-      </div>
-    );
-  }
+	state = { data: [], perPage: 50, currentPage: 0 };
+	componentWillMount() {
+		axios
+			.get(url, settings)
+			.then(response => {
+				let res = response.data.data;
+				res = res.filter(i => {
+					if (!i.privacy || i.privacy === "hidden") {
+						return true;
+					} else {
+						return true;
+					}
+				});
+				this.props.handleClick(res);
+				// this.setState(this.state.data, response.data.data)
+			})
+			.catch(function(error) {
+				console.log("Request failed", error);
+			});
+	}
+	render() {
+		const { datas } = this.props;
+		console.log(datas[0]);
+		return (
+			<div>
+				{datas[0] &&
+					datas[0].map((i, key) => {
+						if (key > this.state.perPage * (this.state.currentPage + 1)) {
+							return;
+						}
+						return (
+							<img
+								src={(i.images && i.images[0].link) || i.link}
+								height="200"
+								key={key}
+							/>
+						);
+					})}
+			</div>
+		);
+	}
 }
 
 const mapDispatchToProps = dispatch => ({
-  handleClick: data => {
-    return dispatch({
-      type: "CLICK",
-      payload: data
-    });
-  }
+	handleClick: data => {
+		return dispatch({
+			type: "CLICK",
+			payload: data
+		});
+	}
 });
 
 const mapStateToProps = state => ({
-  datas: state.reducer1
+	datas: state.reducer1
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
